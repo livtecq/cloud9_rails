c0d35be80846e161d344cd2599383cf14af15389
EC2 Default User <ec2-user@ip-172-31-8-75.ap-northeast-1.compute.internal>
2021-11-15 18:34:53 +0000

    ルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  get 'users/index'
+  # get 'users/index'
+  # 上記は下記の省略形
+  # get 'users/index', to: 'users#index'
+  # /usersへアクセスした時にindexアクションを呼ぶ。
+  get 'users', to: 'users#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
